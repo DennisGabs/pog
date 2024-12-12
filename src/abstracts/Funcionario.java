63b51a7b76747043df388300625043c63f246c76
DENNIS GABRIEL SILVA ALVES <20232ADS.CNT0013@ifma.edu>
2024-12-12 20:17:34 -0300

    add abstracts

---
+++ src/abstracts/Funcionario.java
@@ -0,0 +1,17 @@
+package abstracts;
+
+public class Funcionario {
+    static double salarioTotal;
+    private String nome;
+    private double salario;
+
+    public Funcionario(String nome, double salario){
+        this.nome = nome;
+        this.salario = salario;
+        mudaSalario(this);
+    }
+
+    private void mudaSalario(Funcionario funcionario){
+        salarioTotal += funcionario.salario;
+    }
+}
---
+++ src/abstracts/Main.java
@@ -0,0 +1,14 @@
+package abstracts;
+
+public class Main {
+    public static void main(String[] args) {
+         // Funcionario funcionario =
+        new Funcionario("Dennis", 20000.5);
+        System.out.println(Funcionario.salarioTotal);
+
+        // Funcionario funcionario2 =
+        new Funcionario("Gabriel", 50000.0);
+        System.out.println(Funcionario.salarioTotal);
+
+    }
+}
